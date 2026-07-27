322e6e65a46d7545f3c2ab05382e2ee3ad6fca16
brkcrk20 <brkcrk20@gmail.com>
2026-07-27 07:16:50 +0000

    HERO bölümünde görsel boyutlandırma ve düzen güncellemeleri

--- app/(site)/products/[slug]/page.tsx
+++ app/(site)/products/[slug]/page.tsx
@@ -164,14 +164,14 @@ export default function ProductDetailPage() {
     <div className="w-full min-h-screen bg-white relative font-montserrat text-[#3A3A3A] overflow-x-hidden">
  
       {/* 1. BÖLÜM: HERO & GENEL BAKIŞ */}
-      <section className="w-full min-h-[600px] sm:min-h-[680px] md:min-h-screen relative flex flex-col justify-start px-6 lg:pl-16 lg:pr-16 pt-28 lg:pt-32 pb-20 overflow-hidden bg-[#3A3A3A]">
-        <div className="absolute inset-0 w-full h-full z-0">
+      <section className="w-full min-h-[650px] lg:h-[85vh] relative flex flex-col justify-start px-6 lg:pl-16 lg:pr-16 pt-28 lg:pt-32 pb-20 overflow-hidden bg-[#3A3A3A]">
+        <div className="absolute inset-0 w-full h-full z-0 overflow-hidden">
           <Image
             src="/Positron_2.png"
             alt="Positron Cyclic Oven"
             fill
             sizes="100vw"
-            className="object-cover object-[75%_center] md:object-[87%_center] lg:object-right pt-0 opacity-100"
+            className="w-full h-full object-cover object-[75%_center] md:object-[87%_center] lg:object-right opacity-100"
             priority
           />
         </div>
